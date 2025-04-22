d1584979d59cbe03a680a4f6288c3238e5e118cc
deepak76901 <deepakb76901@gmail.com>
2025-04-22 17:12:47 +0530

    error fixed

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const app: express.Application = express();
 
 const port: number = 3000;
 
-app.get("/", (_req, _res) => {
-  _res.send("TypeScript With Deepak");
+app.get("/", (_req, res) => {
+  res.send("TypeScript With Deepak");
 });
 
 // Server setup
